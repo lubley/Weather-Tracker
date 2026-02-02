171fc22777c535a3c08aaa29b43df73050dd80b8
lubley <amtullubna06@gmail.com>
2026-02-02 13:11:31 +0000

    adding inputs and displaying a mock temperature for now

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def home():
-    return "Hello! My Weather App is running 🌦️"
+    city = request.args.get("city")
+    weather = None
+    if city:
+        # Temporary: mock data for now just for testing.
+        weather = {"temp": 25, "desc": "Sunny"}
+    return render_template("index.html", weather=weather)
 
 if __name__ == "__main__":
     app.run(debug=True)
